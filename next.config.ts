53a054613b4f940ad7c40fb19c2f714931e908ce
SOURIN ROM <sourinrom@gmail.com>
2025-06-30 11:21:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
     ],
   },
